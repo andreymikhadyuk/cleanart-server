b9da221a1169e32f0e6dce6a2d7c0675e83018b0
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2018-10-21 19:24:57 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const logger = require('morgan');
+const appConfig = require('config').get('app');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+// require('./models');
+
+app.use('/', require('./routes'));
+
+server.listen(appConfig.port, () => {
+  console.log(`App started on port ${appConfig.port}`);
+});
